8b445ebafa4d916589e41d31b15e7a2cad78231a
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2019-04-22 15:21:19 +0300

    Use stderr for logger interface
    CloudWatch Logs API behavior is not predictable in this context

--- src/serverless_logger.erl
+++ src/serverless_logger.erl
@@ -12,11 +12,7 @@
 ]).
 
 %%
--record(state, {
-   group  = undefined :: _,
-   stream = undefined :: _,
-   events = undefined :: _
-}).
+-record(state, {}).
 
 %%-----------------------------------------------------------------------------
 %%
@@ -27,7 +23,6 @@ log(Type, Pid, Msg) ->
    pipe:call(?MODULE, {os:timestamp(), Type, Pid, Msg}, infinity).
 
 log_(Type, Pid, Msg) ->
-   % pipe:call(?MODULE, {os:timestamp(), Type, Pid, Msg}).
    pipe:send(?MODULE, {os:timestamp(), Type, Pid, Msg}).
 
 resume() ->
@@ -48,15 +43,7 @@ init(_) ->
    [either ||
       cats:unit( erlang:process_flag(trap_exit, true) ),
       serverless_logger_std:start(),
-      Group  <- env("AWS_LAMBDA_LOG_GROUP_NAME"),
-      Stream <- env("AWS_LAMBDA_LOG_STREAM_NAME"),
-      cats:unit(handle,
-         #state{
-            group  = Group,
-            stream = Stream,
-            events = q:new()
-         }
-      )
+      cats:unit(handle, #state{})
    ].
 
 free(_, _) ->
@@ -67,46 +54,21 @@ free(_, _) ->
 %% state machine
 %%
 %%-----------------------------------------------------------------------------
-handle({T, Type, Pid, Msg}, _, #state{events = Events} = State) ->
+handle({T, Type, Pid, Msg}, _, #state{} = State) ->
    io:fwrite(standard_error, message(Type, Pid, Msg), []),
    {reply, ok, State};
-   % {reply, ok,
-   %    State#state{
-   %       events = q:enq(
-   %          #{
-   %             message => message(Type, Pid, Msg), 
-   %             timestamp => milliseconds(T)
-   %          }, 
-   %          Events
-   %       )
-   %    }
-   % };
 
 handle(resume, _, #state{} = State) ->
    {reply, ok, State};
 
 handle(suspend, _, #state{} = State) ->
-   suspend(State),
-   {reply, ok, State#state{events = q:new()}}.
+   {reply, ok, State}.
   
 %%-----------------------------------------------------------------------------
 %%
 %% private
 %%
 %%-----------------------------------------------------------------------------
-
-%%
-env(Var) ->
-   case os:getenv(Var) of
-      false ->
-         {ok, undefined};
-      Value ->
-         {ok, erlang:iolist_to_binary(Value)}
-   end.
-
-%%
-token(Spec, _) ->
-   lens:get(lens:c(lens:hd(), lens:pair(<<"uploadSequenceToken">>)), Spec).
 
 %%
 milliseconds({A, B, C}) ->
@@ -129,37 +91,3 @@ message(Type, Pid, Msg) ->
    erlang:iolist_to_binary(
       io_lib:format("[~s] ~p: ~p", [Type, Pid, Msg])
    ).
-
-
-%%
-publish(#state{group = undefined, stream = undefined, events = Events}) ->
-   lists:foreach(
-      fun(#{message := Message}) -> 
-         file:write(standard_error, Message)
-      end,
-      q:list(Events)
-   );
-
-publish(#state{group = Group, stream = Stream, events = Events}) ->
-   [either ||
-      Config <- erlcloud_aws:auto_config(),
-      erlcloud_cloudwatch_logs:describe_log_streams(Group, Stream, Config),
-      cats:unit(token(_, _)),
-      erlcloud_cloudwatch_logs:put_logs_events(Group, Stream, _, q:list(Events), Config)
-   ].  
-
-%%
-suspend(#state{events = ?queue()}) ->
-   ok;
-suspend(#state{} = State) ->
-   case publish(State) of
-      {error, {http_error, 400, _, Reason}} ->
-         case jsx:decode(Reason, [return_maps]) of
-            #{<<"__type">> := <<"InvalidSequenceTokenException">>} ->
-               suspend(State);
-            _ ->
-               ok
-         end;
-      _ ->
-         ok
-   end.
