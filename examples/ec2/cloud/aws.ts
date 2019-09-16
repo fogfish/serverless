1213e9b1b5da2b063b4cd3a4b815240d9c01b055
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2019-09-16 22:26:02 +0300

    Use aws-cdk-pure for example stack definitions

--- examples/ec2/cloud/aws.ts
+++ examples/ec2/cloud/aws.ts
@@ -2,46 +2,55 @@ import * as cdk from '@aws-cdk/core'
 import * as iam from '@aws-cdk/aws-iam'
 import * as lambda from '@aws-cdk/aws-lambda'
 import * as logs from '@aws-cdk/aws-logs'
-import { _, $ } from './pure'
+import { IaaC, iaac, root, join, use } from 'aws-cdk-pure'
+
+const f = iaac(lambda.Function)
+const role = iaac(iam.Role)
 
 //
 //
-function Lambda(parent: cdk.Construct): lambda.Function {
-  return new lambda.Function(parent, 'EC2',
-    {
-      description: 'Erlang AWS Lambda - EC2 API Example',
-      runtime: lambda.Runtime.PROVIDED,
-      code: new lambda.AssetCode('../_build/default/bin'),
-      handler: 'index.handler',
-      role: Role(parent),
-      timeout: cdk.Duration.seconds(10),
-      memorySize: 256,
-      logRetention: logs.RetentionDays.FIVE_DAYS,
-      layers: [
-        lambda.LayerVersion.fromLayerVersionArn(parent, 'Layer', 
-          `arn:aws:lambda:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:layer:erlang-serverless:3`)
-      ]
-    }
-  )
+function Lambda(parent: cdk.Construct): lambda.FunctionProps {
+  return {
+    description: 'Erlang AWS Lambda - EC2 API Example',
+    runtime: lambda.Runtime.PROVIDED,
+    code: new lambda.AssetCode('../_build/default/bin'),
+    handler: 'index.handler',
+    role: Role()(parent),
+    timeout: cdk.Duration.seconds(10),
+    memorySize: 256,
+    logRetention: logs.RetentionDays.FIVE_DAYS,
+    layers: [
+      lambda.LayerVersion.fromLayerVersionArn(parent, 'Layer', 
+        `arn:aws:lambda:${cdk.Aws.REGION}:${cdk.Aws.ACCOUNT_ID}:layer:erlang-serverless:3`)
+    ]
+  }
 }
 
 //
 //
-function Role(parent: cdk.Construct): iam.Role {
-  const role = new iam.Role(parent, 'Role',
-    {
-      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
-    }
-  )
-  role.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ReadOnlyAccess"))
-  return role
+function Role(): IaaC<iam.Role> {
+  return use({role: role(Principal)})
+    .effect(x => x.role.addManagedPolicy(Allow()))
+    .yield('role')
 }
 
+function Principal(): iam.RoleProps {
+  return {
+    assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com')
+  }
+}
+
+function Allow(): iam.IManagedPolicy {
+  return iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonEC2ReadOnlyAccess")
+}
+
+//
+//
 function ExampleErlEC2(stack: cdk.Construct): cdk.Construct {
-  _(stack, Lambda)
+  join(stack, f(Lambda))
   return stack
 }
 
 const app = new cdk.App()
-_(app, ExampleErlEC2)
+root(app, ExampleErlEC2)
 app.synth()
